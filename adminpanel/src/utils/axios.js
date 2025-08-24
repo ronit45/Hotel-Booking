8ec5535fd5600fccca48a77fee5c5e69f8271faf
ronit45 <banron@gmail.com>
2025-08-25 02:53:09 +0530

    axios fix

--- adminpanel/src/utils/axios.js
+++ adminpanel/src/utils/axios.js
@@ -1,7 +1,6 @@
-import axios from "axios";
-
 import axios from 'axios';
 
+// Configure global axios defaults for the admin panel
 axios.defaults.baseURL = 'https://hotel-booking-0rkp.onrender.com/api';
 axios.defaults.withCredentials = true;
 
