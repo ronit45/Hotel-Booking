718414474a1aa2e00f1537a03e3947ff835339fc
ronit45 <banron@gmail.com>
2025-08-25 18:29:06 +0530

    debugging

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -5,6 +5,7 @@ import { User } from "../models/user.models.js";
 export const verifyJWT = asyncHandler( async(req,res,next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.
+        console.log(token)
         replace("Bearer ","")
 
         if(!token){
