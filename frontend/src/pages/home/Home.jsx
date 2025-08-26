22a9c42045764878cd100a6b7c9a54308079ef60
ronit45 <banron@gmail.com>
2025-08-26 23:02:41 +0530

    fix ui

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -11,17 +11,17 @@ const Home = () => {
   return (
     <div className="homeRoot">
       <Navbar />
-      <main className="homeContainer">
+      <div className='hero'>
         <h1 className='title'>Find the right hotel today</h1>
         <p className='subtitle'>Search and compare prices from top travel sites.</p>
         <Header />
+      </div>
+      <main className="homeContainer">
         <Featured />
         <section className="homeSection">
-          <h2 className="homeTitle">Browse by property type</h2>
           <PropertyList />
         </section>
         <section className="homeSection">
-          <h2 className="homeTitle">Homes guests love</h2>
           <FeaturedProperties />
         </section>
       </main>
